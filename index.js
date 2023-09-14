ba7d35faaf0d58263be84e19b4192686f57f1340
Skyzzen31 <skyzzen31@gmail.com>
2023-09-14 09:12:29 +0200

    Mobile menu & Animation mobile

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const animationName = document.querySelector(".anim-name");
 const navbar = document.querySelector("nav");
 const brushAnim1 = document.getElementById("anim-1");
-const brushAnim2 = document.getElementById("anim-2");
 const brushAnim3 = document.getElementById("anim-3");
 const brushAnim4 = document.getElementById("anim-4");
 const brushAnim5 = document.getElementById("anim-5");
@@ -31,160 +30,163 @@ const project5 = document.querySelector(".project-5");
 const contactP = document.getElementById("contact-p");
 const emailRedirect = document.querySelector(".email");
 const footer = document.getElementById("footer");
+const modal = document.querySelector(".modal-hide");
+const nav = document.querySelector(".nav-animation");
 
 // ---------------- ANIMATION ON LOAD ---------------
 
 window.addEventListener("load", () => {
-  setTimeout(() => {
-    iconTerminal.style.transform = "translate(-200px, -60px)";
-    iconCamera.style.transform = "translate(200px, -60px)";
-    icon.forEach((icon) => {
-      icon.style.opacity = "1";
-    });
-  }, 400);
+  if (document.documentElement.clientWidth > 750) {
+    setTimeout(() => {
+      iconTerminal.style.transform = "translate(-200px, -60px)";
+      iconCamera.style.transform = "translate(200px, -60px)";
+      icon.forEach((icon) => {
+        icon.style.opacity = "0.3";
+      });
+    }, 400);
 
-  setTimeout(() => {
-    navbar.style.opacity = "1";
-  }, 600);
+    setTimeout(() => {
+      navbar.style.opacity = "1";
+    }, 600);
 
-  setTimeout(() => {
-    iconFolder.style.transform = "translate(160px, -160px)";
-    iconBracket.style.transform = "translate(-120px, -160px)";
-  }, 410);
+    setTimeout(() => {
+      iconFolder.style.transform = "translate(160px, -160px)";
+      iconBracket.style.transform = "translate(-120px, -160px)";
+    }, 410);
 
-  setTimeout(() => {
-    iconObject.style.transform = "translate(-170px, -100px)";
-    iconSocial.style.transform = "translate(220px, -120px)";
-  }, 420);
+    setTimeout(() => {
+      iconObject.style.transform = "translate(-170px, -100px)";
+      iconSocial.style.transform = "translate(220px, -120px)";
+    }, 420);
 
-  setTimeout(() => {
-    animationName.style.height = "183px";
-  }, 500);
-  setTimeout(() => {
-    brushAnim1.style.width = "70px";
-  }, 400);
+    setTimeout(() => {
+      animationName.style.height = "183px";
+    }, 500);
+    setTimeout(() => {
+      brushAnim1.style.width = "134px";
+    }, 400);
+  } else {
+    setTimeout(() => {
+      animationName.style.height = "81px";
+    }, 500);
+    setTimeout(() => {
+      brushAnim1.style.width = "70px";
+    }, 400);
 
-  setTimeout(() => {
-    brushAnim2.style.width = "90px";
-  }, 400);
+    setTimeout(() => {
+      iconTerminal.style.transform = "translate(-95px, -20px)";
+      iconCamera.style.transform = "translate(35px, -55px)";
+      icon.forEach((icon) => {
+        icon.style.opacity = "0.3";
+      });
+    }, 400);
+
+    setTimeout(() => {
+      iconFolder.style.transform = "translate(60px, -270px)";
+      iconBracket.style.transform = "translate(-100px, -250px)";
+    }, 410);
+
+    setTimeout(() => {
+      iconObject.style.transform = "translate(-100px, -80px)";
+      iconSocial.style.transform = "translate(95px, -138px)";
+    }, 420);
+  }
 });
 
 // -------------------ANIMATION ON SCROLL ------------------
 
-if (document.documentElement.clientWidth > 750) {
-  window.addEventListener("scroll", function () {
-    if (window.scrollY >= 306) {
-      brushAnim3.style.width = "108px";
-    }
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 306) {
+    brushAnim3.style.width = "108px";
+  }
 
-    if (window.scrollY >= 386) {
-      aboutText.style.opacity = "1";
-    }
+  if (window.scrollY >= 386) {
+    aboutText.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 1016) {
-      socialCards.style.opacity = "1";
-    }
+  if (window.scrollY >= 1016) {
+    socialCards.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 1632) {
-      brushAnim4.style.width = "108px";
-    }
+  if (window.scrollY >= 1632) {
+    brushAnim4.style.width = "90px";
+  }
 
-    if (window.scrollY >= 1734) {
-      skillsWebDev.style.opacity = "1";
-    }
+  if (window.scrollY >= 1734) {
+    skillsWebDev.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 1938) {
-      skillsGrowth.style.opacity = "1";
-    }
+  if (window.scrollY >= 1938) {
+    skillsGrowth.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 2142) {
-      skillsEditing.style.opacity = "1";
-    }
+  if (window.scrollY >= 2142) {
+    skillsEditing.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 2552) {
-      brushAnim5.style.width = "109px";
-    }
-    if (window.scrollY >= 2552) {
-      project5.style.transform = "translateX(0px)";
-      project5.style.opacity = "1";
-    }
+  if (window.scrollY >= 2552) {
+    brushAnim5.style.width = "109px";
+  }
+  if (window.scrollY >= 2552) {
+    project5.style.transform = "translateX(0px)";
+    project5.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 3200) {
-      project4.style.transform = "translateX(0px)";
-      project4.style.opacity = "1";
-    }
+  if (window.scrollY >= 3200) {
+    project4.style.transform = "translateX(0px)";
+    project4.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 3670) {
-      project3.style.transform = "translateX(0px)";
-      project3.style.opacity = "1";
-    }
+  if (window.scrollY >= 3670) {
+    project3.style.transform = "translateX(0px)";
+    project3.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 4133) {
-      project2.style.transform = "translateX(0px)";
-      project2.style.opacity = "1";
-    }
+  if (window.scrollY >= 4133) {
+    project2.style.transform = "translateX(0px)";
+    project2.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 4644) {
-      project1.style.transform = "translateX(0px)";
-      project1.style.opacity = "1";
-    }
+  if (window.scrollY >= 4644) {
+    project1.style.transform = "translateX(0px)";
+    project1.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 5100) {
-      brushAnim6.style.width = "140px";
-    }
+  if (window.scrollY >= 5100) {
+    brushAnim6.style.width = "140px";
+  }
 
-    if (window.scrollY >= 5604) {
-      contactP.style.opacity = "1";
-    }
+  if (window.scrollY >= 5604) {
+    contactP.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 6108) {
-      emailRedirect.style.opacity = "1";
-    }
+  if (window.scrollY >= 6108) {
+    emailRedirect.style.opacity = "1";
+  }
 
-    if (window.scrollY >= 5608) {
-      footer.style.opacity = "1";
-    }
-  });
-} else {
-  brushAnim3.style.width = "108px";
+  if (window.scrollY >= 5608) {
+    footer.style.opacity = "1";
+  }
+});
 
-  aboutText.style.opacity = "1";
+const handleMove = (event) => {
+  let mouseX, mouseY;
 
-  brushAnim4.style.width = "108px";
+  if (event.type === "mousemove") {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+  } else if (event.type === "touchmove") {
+    event.preventDefault();
+    mouseX = event.touches[0].clientX;
+    mouseY = event.touches[0].clientY;
+  }
 
-  skillsWebDev.style.opacity = "1";
-
-  skillsGrowth.style.opacity = "1";
-
-  skillsEditing.style.opacity = "1";
-  socialCards.style.opacity = "1";
-
-  brushAnim5.style.width = "109px";
-  project5.style.transform = "translateX(0px)";
-  project5.style.opacity = "1";
-  project4.style.transform = "translateX(0px)";
-  project4.style.opacity = "1";
-
-  project3.style.transform = "translateX(0px)";
-  project3.style.opacity = "1";
-
-  project2.style.transform = "translateX(0px)";
-  project2.style.opacity = "1";
-
-  project1.style.transform = "translateX(0px)";
-  project1.style.opacity = "1";
-
-  brushAnim6.style.width = "197px";
-
-  contactP.style.opacity = "1";
-
-  emailRedirect.style.opacity = "1";
-
-  footer.style.opacity = "1";
-}
-document.addEventListener("mousemove", function (event) {
-  let mouseX = event.clientX;
-  let mouseY = event.clientY;
+  let scaleFactor;
+  if (window.innerWidth <= 768) {
+    scaleFactor = 0.5;
+  } else {
+    scaleFactor = 0.5;
+  }
 
   let zoneWidth = document.documentElement.clientWidth;
   let zoneHeight = document.documentElement.clientHeight;
@@ -218,25 +220,46 @@ document.addEventListener("mousemove", function (event) {
 
   // -------- RIGHT-ICON ------------
 
-  iconFolder.style.transform = `translate(${160 * mouseXmultiplicatorR}px, ${
-    -160 * -mouseYmultiplicatorR
-  }px)`;
-  iconSocial.style.transform = `translate(${220 * mouseXmultiplicatorR}px, ${
-    -120 * -mouseYmultiplicatorR
-  }px)`;
-  iconCamera.style.transform = `translate(${200 * mouseXmultiplicatorR}px, ${
-    -60 * -mouseYmultiplicatorR
-  }px)`;
+  iconFolder.style.transform = `translate(${
+    160 * mouseXmultiplicatorR * scaleFactor
+  }px, ${-460 * -mouseYmultiplicatorR * scaleFactor}px)`;
+  iconSocial.style.transform = `translate(${
+    180 * mouseXmultiplicatorR * scaleFactor
+  }px, ${-230 * -mouseYmultiplicatorR * scaleFactor}px)`;
+  iconCamera.style.transform = `translate(${
+    80 * mouseXmultiplicatorR * scaleFactor
+  }px, ${-120 * -mouseYmultiplicatorR * scaleFactor}px)`;
 
   // -----------LEFT-ICON -----------------
 
-  iconBracket.style.transform = `translate(${-120 * -mouseXmultiplicatorL}px, ${
-    -160 * -mouseYmultiplicatorL
-  }px)`;
-  iconObject.style.transform = `translate(${-170 * -mouseXmultiplicatorL}px, ${
-    -100 * -mouseYmultiplicatorL
-  }px)`;
+  iconBracket.style.transform = `translate(${
+    -160 * -mouseXmultiplicatorL * scaleFactor
+  }px, ${-420 * -mouseYmultiplicatorL * scaleFactor}px)`;
+  iconObject.style.transform = `translate(${
+    -170 * -mouseXmultiplicatorL * scaleFactor
+  }px, ${-140 * -mouseYmultiplicatorL * scaleFactor}px)`;
   iconTerminal.style.transform = `translate(${
-    -200 * -mouseXmultiplicatorL
-  }px, ${-60 * -mouseYmultiplicatorL}px)`;
-});
+    -180 * -mouseXmultiplicatorL * scaleFactor
+  }px, ${-60 * -mouseYmultiplicatorL * scaleFactor}px)`;
+};
+
+document.addEventListener("mousemove", handleMove);
+document.addEventListener("touchmove", handleMove);
+
+function toggleHamburger() {
+  var menu = document.querySelector(".hamburger-menu");
+  menu.classList.toggle("cross");
+  if (modal.classList.contains("modal-hide")) {
+    modal.classList.remove("modal-hide");
+    nav.classList.remove("fadeOut");
+    modal.classList.add("modal-open");
+    nav.classList.add("fadeIn");
+    document.body.style.overflow = "hidden";
+  } else {
+    modal.classList.remove("modal-open");
+    nav.classList.remove("fadeIn");
+    modal.classList.add("modal-hide");
+    nav.classList.add("fadeOut");
+    document.body.style.overflow = "auto";
+  }
+}
